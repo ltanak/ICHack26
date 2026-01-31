6ed4abfc76d8773679fb3181ee1c6d283492a9c5
Archii0 <archiedascombe@icloud.com>
2026-01-31 16:03:01 +0000

    Add claude api wrapper

---
+++ backend/anthopic.py
@@ -0,0 +1,20 @@
+import anthropic
+
+# Exvironment variable 'ANTHROPIC_API_KEY' needs to be set
+
+client = anthropic.Anthropic()
+
+def sendPrompt(prompt: str, max_tokens: int = 1000) -> str:
+
+    message = client.messages.create(
+        model="claude-sonnet-4-5",
+        max_tokens=max_tokens,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return message.content
