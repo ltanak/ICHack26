a870dca504f6f7714e122a6b45ce1fe2935c5fdf
Nicholas Mason-Apps <nicholas.mason-apps@live.co.uk>
2026-02-01 04:36:45 +0000

    Revamped ui, added ai gen endpoint, adjusted map

---
+++ frontend/src/components/WildfireSummary.js
@@ -0,0 +1,50 @@
+import { Divider } from "antd";
+import { useSelector } from "react-redux";
+import { useGetPointSummaryQuery } from "../store/api";
+import { useEffect, useState } from "react";
+import { useAnimatedText } from "@/app/hooks/useAnimatedText";
+
+export default function WildfireSummary() {
+    const selectedPoint = useSelector((state) => state.points.selectedPoint);
+    const { data: pointSummary, error, isFetching } = useGetPointSummaryQuery(encodeURIComponent(selectedPoint?.name), {
+        skip: !selectedPoint,
+    });
+    // const [textToAnimate, setTextToAnimate] = useState("");
+    const animatedText = useAnimatedText(pointSummary || "");
+
+    if (!selectedPoint) {
+        return (
+            <div className="bg-slate-200 p-4 text-center flex-1">
+                <h2 className="text-3xl font-bold mb-2">No Wildfire Selected</h2>
+                <p className="text-xl">Please select a wildfire on the map to see more information.</p>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            {isFetching && (
+                <div className="flex flex-col items-center justify-center mt-8 animate-fade-in">
+                    <div className="w-8 h-8 border-4 border-slate-300 border-t-indigo-500 rounded-full animate-spin mb-3"></div>
+                    <p className="text-slate-700 text-lg font-medium">
+                        Generating summary...
+                    </p>
+                </div>
+            )}
+            {error && (
+                <p className="text-red-600 mt-6 text-center">
+                    Failed to generate summary.
+                </p>
+            )}
+            {!isFetching && pointSummary&& (
+                <div className="px-32">
+                    <h2 className="text-2xl font-semibold text-center pb-2">Summary</h2>
+                    <div className="text-left text-lg">
+                        {animatedText}
+                        <span className="inline-block w-2 animate-pulse">▌</span>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
